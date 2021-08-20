854d698996653cc7226f2127d57c8f613e21352f
Evgeny Cherkashin <echerkashin@digital-spirit.ru>
2021-08-20 10:07:49 +0300

    added chip icon

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,13 +1,16 @@
 <template lang="html">
-    <div>
-        
-    </div>
+  <div>
+    <chip side="white"></chip>
+  </div>
 </template>
 <script>
+import Chip from "./Chip.vue";
+
 export default {
-    
-}
+  components: {
+    chip: Chip,
+  },
+};
 </script>
 <style lang="css">
-    
 </style>
--- src/components/Chip.vue
+++ src/components/Chip.vue
@@ -1,13 +1,39 @@
 <template lang="html">
-    <div>
-        
-    </div>
+    <v-img 
+      class="chip"
+      :src="src"
+    ></v-img>
 </template>
 <script>
 export default {
-    
-}
+  props: {
+    side: {
+      type: String,
+      required: true,
+      validator: function (value) {
+        return ["white", "black"].includes(value);
+      },
+    },
+    type: {
+      type: String,
+      default: function () {
+        return "common";
+      },
+      validator: function (value) {
+        return ["common"].includes(value);
+      },
+    },
+  },
+  computed: {
+    src: function () {
+      return require(`@/assets/chips/${this.type}/${this.side}.svg`);
+    },
+  },
+};
 </script>
 <style lang="css">
-    
+.chip {
+  min-width: 20px;
+  min-height: 20px;
+}
 </style>
